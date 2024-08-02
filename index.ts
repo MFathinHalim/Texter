955197915536ba9abaca3c0d72a6018ff3e313d9
MFathinHalim <halimfathin7@gmail.com>
2024-08-02 19:21:56 +0700

    tambahin comment

--- index.ts
+++ index.ts
@@ -9,32 +9,35 @@ import router from "./Router/main";
 //? dotenv config
 dotenv.config();
 
-const app: Express = express(); //bikin expressnya
-const port: number | string = process.env.PORT || 3000; //bikin port :D
+const app: Express = express(); //TODO bikin expressnya
+const port: number | string = process.env.PORT || 3000; //TODO bikin port :D
 
-app.set("view engine", "ejs"); //set view enginenya jadi ejs
+app.set("view engine", "ejs"); //? set view enginenya jadi ejs
 app.use(express.static(path.join(__dirname, "/public"))); //TODO buat frontendnya, css js image di taruh di public
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//Rate Limit
 const rateLimit = require("express-rate-limit");
 const limiter = rateLimit({
-  windowMs: 5 * 60 * 1000, // 15 menit
-  max: 5, // maksimal 1 request setiap 15 menit
-  message: "Try Again Later:D.",
+  windowMs: 5 * 60 * 1000, //? 5 menit
+  max: 5,
+  message: "Coba lagi 5 menit",
 });
 const limiterSignup = rateLimit({
-  windowMs: 60 * 60 * 1000, // 15 menit
-  max: 1, // maksimal 1 request setiap 15 menit
-  message: "Coba lagi 15 menit.",
+  windowMs: 60 * 60 * 1000, //? 1 jam
+  max: 1,
+  message: "Coba lagi 1 jam",
 });
-app.use("/post", limiter);
 function postOnlyLimiter(req: Request, res: Response, next: NextFunction) {
   if (req.method === "POST") {
     return limiterSignup(req, res, next);
   }
   next();
 }
+// uset post limit
+app.use("/post", limiter);
 app.use("/signup", postOnlyLimiter);
 
 //? Jalankan Routernya
